5d08315c9de825ba60ffed531da3f8d165865568
ÐSergey Ivanov <director51460mr@gmail.com>
2022-12-20 21:29:50 +0300

    save

---
+++ task.js
@@ -0,0 +1,52 @@
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+}
+
+Student.prototype.setSubject = function (subjectName) {
+  this.subjectName = subjectName;
+}
+
+Student.prototype.addMark = function (mark) {
+    if(this.marks === undefined){ 
+    this.mark = [mark];
+    } else {
+    this.mark.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function () {
+  const getAverage = (marks) => {
+    let sum = 0; 
+    for (let i = 0; i < marks.length; i += 1) { 
+      sum += marks[i]; 
+    }
+    return sum / marks.length;
+  };
+}
+
+Student.prototype.exclude = function(reason){
+  delete this.subject;
+  delete this.marks;
+  this.reason = reason;
+  }
+ let student1 = new Student("Tony", "male", 37);
+ student1.setSubject("Algebra");
+ student1.addMark(5);
+ student1.addMark(4);
+ student1.addMark(5);
+ console.log(student1.getAverage()); 
+ console.log(student1);
+ let student2 = new Student("Buzz", "female", 35);
+ student2.setSubject("Geometry");
+ student2.addMark(2);
+ student2.addMark(3);
+ student2.addMark(2);
+ student2.exclude('low grades')
+ console.log(student2)
+  
